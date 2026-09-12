6b69a767f12e45d920de2cf10966a263c8362f69
vikash893504-ctrl <vikash893504@gmail.com>
2026-09-13 02:18:41 +0530

    Delete plugins directory

--- plugins/start.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message
-
-# Har tarah ke private message par reply karega (Command ho ya na ho)
-@Client.on_message(filters.private)
-async def catch_all(bot: Client, message: Message):
-    print(f"MESSAGE RECEIVED: {message.text}")
-    await message.reply_text(f"Received: {message.text}")
-  
